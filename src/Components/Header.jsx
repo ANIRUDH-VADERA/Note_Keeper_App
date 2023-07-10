436b061e50ed6d6616cf9824e26de97f2ab1f69d
ANIRUDH-VADERA <anirudhvadera@gmail.com>
2023-07-10 13:01:54 +0530

    Final

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import logo from "../images/adglogo2 1.svg";
 
 function Header() {
   return (
       <Link to="/Welcome">
         <header>
-            <img src="images/adglogo2 1.svg" alt="ADG Logo" />
+            <img src={logo} alt="ADG Logo" />
             <h1>Keeper</h1>
         </header>
       </Link>
